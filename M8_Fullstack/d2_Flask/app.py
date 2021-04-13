d10fa26dfad05bdbaa70fa9f07100a33e33c892c
lpianta <pianta.luca@gmail.com>
2021-04-13 17:00:53 +0200

    exercise

--- M8_Fullstack/d2_Flask/app.py
+++ M8_Fullstack/d2_Flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
+
 
 app = Flask(__name__)
 
@@ -6,6 +7,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/advice')
+def advice():
+    return render_template('advice.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/bookshelf')
+def bookshelf():
+    return render_template('bookshelf.html')
 
 
 if __name__ == "__main__":
